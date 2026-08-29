26ca916d209ae45ba0b1e4fa09c33cf306455f4a
Eric Gu <161403372+EG433@users.noreply.github.com>
2026-08-28 20:40:07 -0400

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        l, r = 1, 1
+        # 1, 1, 2, 3, 5, 
+        for i in range(n):
+            temp = r
+            r = l
+            l = temp + r
+        return r
